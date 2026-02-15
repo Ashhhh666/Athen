24b24366b26f6ce1b82683f05037b7aa1a5794f4
skies-starred <skies.starred@gmail.com>
2026-02-15 20:57:37 +0530

    feat(RareItemAlert)
    only Skeleton master chestplate for now

---
+++ src/main/kotlin/xyz/aerii/athen/modules/impl/dungeon/RareItemAlert.kt
@@ -0,0 +1,38 @@
+package xyz.aerii.athen.modules.impl.dungeon
+
+import net.minecraft.network.protocol.game.ClientboundTakeItemEntityPacket
+import net.minecraft.world.entity.item.ItemEntity
+import tech.thatgravyboat.skyblockapi.api.datatype.DataTypes
+import tech.thatgravyboat.skyblockapi.api.datatype.getData
+import xyz.aerii.athen.annotations.Load
+import xyz.aerii.athen.annotations.OnlyIn
+import xyz.aerii.athen.api.location.SkyBlockIsland
+import xyz.aerii.athen.config.Category
+import xyz.aerii.athen.handlers.Smoothie.alert
+import xyz.aerii.athen.handlers.Smoothie.client
+import xyz.aerii.athen.handlers.Smoothie.level
+import xyz.aerii.athen.handlers.Texter.parse
+import xyz.aerii.athen.handlers.Typo.modMessage
+import xyz.aerii.athen.handlers.Typo.stripped
+import xyz.aerii.athen.modules.Module
+
+@Load
+@OnlyIn(islands = [SkyBlockIsland.THE_CATACOMBS])
+object RareItemAlert : Module(
+    "Rare item alert",
+    "Alerts you when you get rare items!",
+    Category.DUNGEONS
+) {
+    init {
+        onReceive<ClientboundTakeItemEntityPacket> {
+            val entity = level?.getEntity(itemId) as? ItemEntity ?: return@onReceive
+            if ("Skeleton Master Chestplate" !in entity.item.displayName.stripped()) return@onReceive
+            if (entity.item?.getData(DataTypes.DUNGEON_QUALITY) != 50) return@onReceive
+
+            client.execute {
+                "<red>Rare drop! <yellow>Skeleton Master Chestplate <gray>[Quality=50]".parse().modMessage()
+                "<red>Rare drop!".parse().alert()
+            }
+        }
+    }
+}
