0069bd794bf863e2716b922cb6e407b413c02b14
skies-starred <skies.starred@gmail.com>
2026-02-21 08:28:47 +0000

    feat(BuildInfo): stun notification

--- src/main/kotlin/xyz/aerii/athen/modules/impl/kuudra/BuildInfo.kt
+++ src/main/kotlin/xyz/aerii/athen/modules/impl/kuudra/BuildInfo.kt
@@ -9,8 +9,12 @@ import xyz.aerii.athen.api.kuudra.enums.KuudraPhase
 import xyz.aerii.athen.api.kuudra.enums.KuudraSupply
 import xyz.aerii.athen.api.location.SkyBlockIsland
 import xyz.aerii.athen.config.Category
+import xyz.aerii.athen.events.KuudraEvent
 import xyz.aerii.athen.events.WorldRenderEvent
 import xyz.aerii.athen.events.core.runWhen
+import xyz.aerii.athen.handlers.Smoothie.alert
+import xyz.aerii.athen.handlers.Texter.parse
+import xyz.aerii.athen.handlers.Typo.modMessage
 import xyz.aerii.athen.modules.Module
 import xyz.aerii.athen.ui.themes.Catppuccin
 import xyz.aerii.athen.utils.render.Render2D.sizedText
@@ -25,17 +29,37 @@ object BuildInfo : Module(
     Category.KUUDRA
 ) {
     private val waypoints = config.switch("Unfinished build waypoint", true).custom("waypoints")
-    private val color by config.colorPicker("Color", Color(Catppuccin.Mocha.Red.argb, true))
+    private val color by config.colorPicker("Color", Color(Catppuccin.Mocha.Red.argb, true)).dependsOn { waypoints.value }
+    private val stun by config.switch("Stun notification", true)
+    private val `stun$percent` by config.slider("Notify at", 90, 1, 100, "%").dependsOn { stun }
+    private val `stun$message` by config.textInput("Notification message", "<red>Stun!").dependsOn { stun }
+
+    private var sent: Boolean = false
 
     private val render: Boolean
         get() = KuudraAPI.inRun && KuudraAPI.phase == KuudraPhase.Build
 
     init {
+        KuudraAPI.buildProgress.onChange {
+            if (!stun) return@onChange
+            if (sent) return@onChange
+            if (it <= `stun$percent`) return@onChange
+
+            val prs = `stun$message`.parse(true)
+            prs.alert()
+            prs.modMessage()
+            sent = true
+        }
+
         config.hud("Build info") {
             if (it) return@hud sizedText("§7Builders: §c3\n§7Progress: §c47%")
             if (!render) return@hud null
 
             sizedText("§7Builders: §c${KuudraAPI.buildPlayers}\n§7Progress: §c${KuudraAPI.buildProgress}%")
+        }
+
+        on<KuudraEvent.Start> {
+            sent = false
         }
 
         on<WorldRenderEvent.Extract> {
